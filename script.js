d59393f1f2ef7536170ef86cf6c0a75bff089c57
KurniawanSumiarta <jacatranet.kurniawan@gmail.com>
2026-01-07 23:38:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<script>
 let slides = document.querySelectorAll(".slide");
 let index = 0;
 
@@ -6,4 +7,15 @@ setInterval(() => {
   index = (index + 1) % slides.length;
   slides[index].classList.add("active");
 }, 4000); // ganti tiap 4 detik
+</script>
 
+<script>
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 4000);
+</script>
